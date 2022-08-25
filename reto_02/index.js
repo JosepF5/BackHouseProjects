3544f7482d701d3910388cca15b35ffb9f074edc
Josep Palomino <jfpalomino.22@gmail.com>
2022-08-25 11:45:56 -0500

    reto_02

--- reto_02/index.js
+++ reto_02/index.js
@@ -7,7 +7,6 @@ class Contenedor{
     }
     async escribir(arreglo){
         const nTexto=JSON.stringify(arreglo,null,'\t')
-        console.log(nTexto)
         await fs.promises.writeFile('./productos.txt',nTexto)
     }
     save(objeto){
@@ -24,25 +23,22 @@ class Contenedor{
     deleteById(id){
         let index = arreglo.findIndex(data => data.id === id)
         arreglo.splice(index, 1)
-        console.log("arreglo en delete",arreglo)
         this.escribir(arreglo)
     }
     deleteAll(){
         arreglo.splice(0,arreglo.length)
-        console.log("arreglo en delete all",arreglo)
         this.escribir(arreglo)
     }
 }
-console.log(arreglo)
 //Creacion de objeto Contenedor
 nuevo= new Contenedor()
 //Metodo Save()
-nuevo.save({nombre:"Book",price:234,thumbnail:"https://cdn2.iconfinder.com/data/icons/school-supplies-7/64/164_school-open-study-education-book-library-512.png"})
+//-->nuevo.save({nombre:"Book",price:234,thumbnail:"https://cdn2.iconfinder.com/data/icons/school-supplies-7/64/164_school-open-study-education-book-library-512.png"})
 //Metodo getById(number)
-//console.log("---GetById---\n",nuevo.getById(3))
+//-->console.log("---GetById---\n",nuevo.getById(3))
 //Metodo getAll()
-//console.log("---GetAll---\n",nuevo.getAll())
+//-->console.log("---GetAll---\n",nuevo.getAll())
 //Metodo deleteById()
-nuevo.deleteById(4)
+//-->nuevo.deleteById(4)
 //Metodo deleteAll()
-//nuevo.deleteAll()
+//-->nuevo.deleteAll()
