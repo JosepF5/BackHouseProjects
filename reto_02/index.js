9dcfadfaf72bd0f487ed7eb507b684babd158ad2
Josep Palomino <jfpalomino.22@gmail.com>
2022-08-29 21:17:10 -0500

    Correcion reto v1.0

--- reto_02/index.js
+++ reto_02/index.js
@@ -1,44 +1,51 @@
-const fs=require('fs')
-let txt=fs.readFileSync('./productos.txt')
-let arreglo=JSON.parse(txt)
+const fs=require('fs');
+
 
 class Contenedor{
-    constructor(){
+    
+    async save(objeto){
+        let txt=await fs.promises.readFile('./productos.txt')
+        let arreglo=JSON.parse(txt)
+        arreglo.push(Object.assign(objeto,{id:arreglo.length+1}))
+        await fs.promises.writeFile('./productos.txt',JSON.stringify(arreglo,null,'\t')) 
+        return `Producto ${objeto.nombre} agregado`
+    } 
+    async getById(id){
+        let txt=await fs.promises.readFile('./productos.txt')
+        let arreglo=JSON.parse(txt)
+        let prod=arreglo.find(el => el.id===id)
+        return prod?prod:"Producto no encontrado."
     }
-    async escribir(arreglo){
-        const nTexto=JSON.stringify(arreglo,null,'\t')
-        await fs.promises.writeFile('./productos.txt',nTexto)
+    async getAll(){
+        let txt=await fs.promises.readFile('./productos.txt')
+        let arreglo=JSON.parse(txt)
+        return arreglo.length!==0?arreglo.map(el => el):"No hay productos."
     }
-    save(objeto){
-        objeto=Object.assign(objeto,{id:arreglo.length+1})
-        arreglo.push(objeto)
-        this.escribir(arreglo)
-    }
-    getById(num){
-        return arreglo.map(el => el)[num-1]
-    }
-    getAll(){
-        return arreglo.map(el => el)
-    }
-    deleteById(id){
+    async deleteById(id){
+        let txt=await fs.promises.readFile('./productos.txt')
+        let arreglo=JSON.parse(txt)
         let index = arreglo.findIndex(data => data.id === id)
-        arreglo.splice(index, 1)
-        this.escribir(arreglo)
+        if(index===id-1){
+            arreglo.splice(index, 1)
+            await fs.promises.writeFile('./productos.txt',JSON.stringify(arreglo,null,'\t'))
+            return "Producto eliminado"
+        }
+        return "No se encontro el producto."
     }
-    deleteAll(){
-        arreglo.splice(0,arreglo.length)
-        this.escribir(arreglo)
-    }
+    async deleteAll(){
+        await fs.promises.writeFile('./productos.txt',"[]")
+        return "Productos eliminados."
+    } 
 }
 //Creacion de objeto Contenedor
 nuevo= new Contenedor()
 //Metodo Save()
-//-->nuevo.save({nombre:"Book",price:234,thumbnail:"https://cdn2.iconfinder.com/data/icons/school-supplies-7/64/164_school-open-study-education-book-library-512.png"})
+//->nuevo.save({nombre:"Book",price:234,thumbnail:"https://cdn2.iconfinder.com/data/icons/school-supplies-7/64/164_school-open-study-education-book-library-512.png"}).then(res=>console.log(res))
 //Metodo getById(number)
-//-->console.log("---GetById---\n",nuevo.getById(3))
+//->nuevo.getById(3).then(res=>console.log(res))
 //Metodo getAll()
-//-->console.log("---GetAll---\n",nuevo.getAll())
+//->nuevo.getAll().then(res=>console.log(res))
 //Metodo deleteById()
-//-->nuevo.deleteById(4)
+//->nuevo.deleteById(3).then(res=>console.log(res))
 //Metodo deleteAll()
-//-->nuevo.deleteAll()
+//->nuevo.deleteAll().then(res=>console.log(res))
